6afecf2a8128661e857fd201f16b29437f49b072
zouk5574-stack <zouk5574@gmail.com>
2025-09-25 20:38:05 +0100

    Create db.js

---
+++ src/src/db.js
@@ -0,0 +1,23 @@
+// src/db.js
+import pg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { Pool } = pg;
+
+if (!process.env.DATABASE_URL) {
+  console.error('DATABASE_URL not set in env');
+  process.exit(1);
+}
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  // ajouter ssl si nécessaire (ex: Supabase)
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+// simple helper to query
+export const query = (text, params) => pool.query(text, params);
+export default pool;
