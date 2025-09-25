f2fce807e80a89cc2cdea78390adedee0c3c172d
zouk5574-stack <zouk5574@gmail.com>
2025-09-25 09:58:46 +0100

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,23 +1,8 @@
-// routes/products.js
 const express = require("express");
-const {
-  getAllProducts,
-  getProduct,
-  createProduct,
-  updateProduct,
-  deleteProduct
-} = require("../controllers/productController");
-const auth = require("../middleware/auth");
-
 const router = express.Router();
+const productController = require("../controllers/productController");
 
-// publiques
-router.get("/", getAllProducts);
-router.get("/:id", getProduct);
-
-// protégées (création/modif/suppression réservées à vendeurs/admin)
-router.post("/", auth, createProduct);
-router.put("/:id", auth, updateProduct);
-router.delete("/:id", auth, deleteProduct);
+router.get("/", productController.getAllProducts);
+router.post("/", productController.createProduct);
 
 module.exports = router;
