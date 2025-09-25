c03759074fc8f2ee2c1a243a389b96c915d6947c
zouk5574-stack <zouk5574@gmail.com>
2025-09-25 10:20:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
 const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
 
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+// Import des routes
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-// Routes principales
+// Utilisation des routes
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 
-// Route test
-app.get("/", (req, res) => {
-  res.send("🚀 API Marketplace opérationnelle !");
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`✅ Serveur démarré sur le port ${PORT}`);
+  console.log(`🚀 Serveur démarré sur le port ${PORT}`);
 });
