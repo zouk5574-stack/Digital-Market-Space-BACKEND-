0066e7dad4f495d577e3fe136706716f40a5cc3e
zouk5574-stack <zouk5574@gmail.com>
2025-09-26 14:50:32 +0100

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,7 @@
-import express from 'express';
-import { register, login, verifyEmail, me } from '../controllers/authController.js';
-import { protect } from '../middleware/auth.js';
+const express = require("express");
 const router = express.Router();
-router.post('/register', register);
-router.get('/verify-email', verifyEmail);
-router.post('/login', login);
-router.get('/me', protect, me);
-export default router;
+const authController = require("../controllers/authController");
+
+router.post("/admin/login", authController.loginAdmin);
+
+module.exports = router;
