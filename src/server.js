6901483059d7be3980934795194a901d6e724b4e
zouk5574-stack <zouk5574@gmail.com>
2025-09-26 16:28:25 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import app from './app.js';
 import dotenv from 'dotenv';
 import './cron/maintenance.js'; // start cron jobs (safe: cron checks CRON_SECRET & env)
 dotenv.config();
-
+import "./jobs/cleanupNotifications.js";
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
