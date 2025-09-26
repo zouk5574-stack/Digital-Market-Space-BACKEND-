7c4136b5721fbe0f0999faae6f465d66476fba14
zouk5574-stack <zouk5574@gmail.com>
2025-09-26 17:53:44 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import notificationRoutes from "./routes/notificationRoutes.js";
 import freelanceServiceRoutes from "./routes/freelanceServiceRoutes.js";
 import freelanceOrderRoutes from "./routes/freelanceOrderRoutes.js";
 import adminSettingsRoutes from "./routes/adminSettingsRoutes.js";
+import "./cron/autoConfirmFreelance.js";
 app.use("/api/admin/settings", adminSettingsRoutes);
 app.use("/api/freelance/services", freelanceServiceRoutes);
 app.use("/api/freelance/orders", freelanceOrderRoutes);
