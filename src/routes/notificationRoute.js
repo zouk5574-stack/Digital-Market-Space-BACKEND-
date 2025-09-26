abe5606608ee11755e2e77eebbfa04391b76bf8f
zouk5574-stack <zouk5574@gmail.com>
2025-09-26 16:24:16 +0100

    Update notificationRoute.js

--- src/routes/notificationRoute.js
+++ src/routes/notificationRoute.js
@@ -1,21 +1,16 @@
-// routes/notificationRoutes.js
 import express from "express";
 import { protect, adminOnly } from "../middleware/auth.js";
-import {
-  createNotification,
-  getUserNotifications,
-  markAsRead
-} from "../controllers/notificationController.js";
+import { sendNotification, getNotifications, markAsRead } from "../controllers/notificationController.js";
 
 const router = express.Router();
 
-// Créer une notification (admin)
-router.post("/", protect, adminOnly, createNotification);
+// Admin envoie une notif à tous
+router.post("/send", protect, adminOnly, sendNotification);
 
-// Récupérer ses notifications (utilisateur connecté)
-router.get("/", protect, getUserNotifications);
+// User récupère ses notifs
+router.get("/", protect, getNotifications);
 
-// Marquer une notification comme lue
-router.post("/read", protect, markAsRead);
+// User marque une notif comme lue
+router.put("/:id/read", protect, markAsRead);
 
 export default router;
