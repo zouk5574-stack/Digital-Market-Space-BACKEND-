acff55ee87848ec5ac8013f040fa3984889c08f8
zouk5574-stack <zouk5574@gmail.com>
2025-09-26 11:10:12 +0100

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,24 +1,19 @@
-// src/db.js
 import pg from 'pg';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 const { Pool } = pg;
 
 if (!process.env.DATABASE_URL) {
-  console.error('DATABASE_URL not set in .env');
+  console.error('DATABASE_URL not set in env');
   process.exit(1);
 }
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // nécessaire pour Supabase
-  },
+  max: Number(process.env.PG_MAX_CLIENTS || 10),
+  ssl: { rejectUnauthorized: false }
 });
 
-// Helper pour exécuter des requêtes SQL
 export const query = (text, params) => pool.query(text, params);
-
 export default pool;
