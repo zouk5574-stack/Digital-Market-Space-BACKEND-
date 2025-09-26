e600dafd971d69692eff6b346cb08799ecfda29d
zouk5574-stack <zouk5574@gmail.com>
2025-09-26 14:59:10 +0100

    Update vercel.js

--- vercel.js
+++ vercel.js
@@ -1,9 +1,21 @@
 {
   "version": 2,
   "builds": [
-    { "src": "src/server.js", "use": "@vercel/node" }
+    {
+      "src": "src/server.js",
+      "use": "@vercel/node"
+    }
   ],
   "routes": [
-    { "src": "/(.*)", "dest": "src/server.js" }
-  ]
+    {
+      "src": "/(.*)",
+      "dest": "src/server.js"
+    }
+  ],
+  "env": {
+    "DATABASE_URL": "@database_url",
+    "JWT_SECRET": "@jwt_secret",
+    "FEDAPAY_API_URL": "@fedapay_api_url",
+    "FEDAPAY_PAYMENT_KEY": "@fedapay_payment_key"
+  }
 }
